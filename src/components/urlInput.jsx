69c31073f2a524a54e51bf60b7f598e4a1d7af0f
Fernando F <ferfig.apps@gmail.com>
2019-11-25 00:11:46 +0000

    storage working properly

--- src/components/urlInput.jsx
+++ src/components/urlInput.jsx
@@ -7,23 +7,49 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 
-const LSKEY = "FFsURLsLStoragr"; //local storage key
+const LSKEY = "FFsURLsStorage"; //local storage key
 
 class UrlShortenner extends Component {
   state = {
     url: "",
-    shortenedUrl: "",
+    shortUrl: "",
+    show: false,
     savedUrls: [] //local storage date (array of JSon {urlShortenned: "tiynUrl", url : "url"})
   };
 
   componentDidMount() {
-    const savedValues = localStorage.getItem(LSKEY);
-    if (savedValues === null) {
+    if (localStorage.getItem(LSKEY) === null) {
       console.log("componentDidMount: there are no Urls saved!");
     } else {
+      let savedValues = JSON.parse(localStorage.getItem(LSKEY));
       console.log("componentDidMount:", JSON.stringify(savedValues));
       this.setState({ savedUrls: savedValues });
     }
+  }
+
+  addToLocalStorage(shortKey) {
+    //get current items
+    let savedValue = localStorage.getItem(LSKEY);
+
+    //add the new one
+    const newValue = [
+      {
+        shortUrl: shortKey,
+        url: this.state.url
+      }
+    ];
+
+    if (savedValue === null) {
+      savedValue = newValue; //JSON object
+    } else {
+      let storedData = JSON.parse(savedValue);
+      savedValue = storedData.concat(newValue);
+    }
+
+    //save it in local storage...
+    localStorage.setItem(LSKEY, JSON.stringify(savedValue));
+    //... and to current state
+    this.setState({ savedUrls: savedValue });
   }
 
   handleTextChange = evt => {
@@ -32,50 +58,28 @@ class UrlShortenner extends Component {
 
   handleSubmit = () => {
     if (this.state.url !== "") {
-      const newShort = this.doTheShortThing(this.state.url);
+      const shortKey = Math.random()
+        .toString(36)
+        .substr(2, 5);
+
       this.setState({
-        shortenedUrl: newShort,
-        show: !this.state.show
+        shortUrl: shortKey,
+        show: true
       });
+
+      this.addToLocalStorage(shortKey);
     } else {
       this.setState({ show: false });
     }
-
-    this.addToLocalStorage();
   };
 
-  addToLocalStorage() {
-    //get current items
-    let savedValue = localStorage.getItem(LSKEY);
-    console.log("Saved value", JSON.stringify(savedValue));
-
-    console.log("NEW short value:", this.state.shortenedUrl);
-    console.log("NEW url value:", this.state.url);
-
-    //add the new one
-    const newValue = [
-      { shortenedUrl: this.state.shortenedUrl, url: this.state.url }
-    ];
-    if (savedValue === null) {
-      savedValue = newValue;
-    } else {
-      savedValue.concat(newValue);
-    }
-    this.setState({ savedUrls: savedValue });
-
-    //save it
-    localStorage.setItem(LSKEY, savedValue);
-    console.log("New stored values", JSON.stringify(savedValue));
-  }
-
-  doTheShortThing(url) {
-    //UrlMap.
-    const urlkey = Math.random()
-
-      .toString(36)
-      .substr(2, 5);
-    return urlkey;
-  }
+  handleReset = () => {
+    this.setState({
+      url: "",
+      shortUrl: "",
+      show: false
+    });
+  };
 
   render() {
     return (
@@ -93,6 +97,7 @@ class UrlShortenner extends Component {
               placeholder="Input Url"
               margin="normal"
               onChange={this.handleTextChange}
+              value={this.state.url}
             />
           </Typography>
         </CardContent>
@@ -114,13 +119,14 @@ class UrlShortenner extends Component {
             Reset
           </Button>
         </CardActions>
-        {this.state.show && this.state.shortenedUrl !== "" && (
+        {this.state.show && this.state.shortUrl !== "" && (
           <CardContent>
             <Typography variant="body2" component="p">
               <label>{this.state.url}</label>
+              <br />
               Shortened to URL:&nbsp;
               <a href={this.state.url} target="_blanc">
-                {this.state.shortenedUrl}
+                {this.state.shortUrl}
               </a>
             </Typography>
           </CardContent>
